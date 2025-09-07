6692fb16244f097a00f318e3f61e615a08a482d0
VKG2009 <vijaygupta79@hotmail.com>
2025-09-08 00:17:01 +0800

    Update MarketPredection.py

--- MarketPredection.py
+++ MarketPredection.py
@@ -119,7 +119,7 @@ plt.title("Feature Correlation Heatmap")
 prediction=data
 
 plt.figure(figsize=(12,6))
-plt.plot(data['date'], data['rsi'],color="blue")
+plt.plot(data['Date'], data['rsi'],color="blue")
 plt.xlabel("date")
 plt.ylabel("rsi")
 plt.title("Price over time")
